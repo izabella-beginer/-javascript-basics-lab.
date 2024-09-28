af30abd1e84e3061dbbffd54df2cd951ce65f099
izabella-beginer <n01716105@humber.ca>
2024-09-27 22:51:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+console.log ("Hello world")
+
+let studentname = "Bella"
+let studentage = 25;
+let BellaEnrolled = true ; 
+
+console.log('studentname is $ {Bella}, age is $ {25} and enrollmentstatues is ${true}');
+
+//string
+let firstName = "Sponge";
+let middleName = 'bob';
+let lastName = `Squarepants`;
+
+console.log("My son's favorite toy is Spongebob Square pants, He doesn't live in bikini bottom ")
+
+//object
+const person2 = {
+ age : 4,
+name: "Abu"
+
+}
+
+console.log (person2.name)
+
+//array
+let fruits= ['cantaloupe', 'watermelon', 'cherry'];
+
+console.log ('hello my name is ${Abu} and i like these fruits ')
+
+let Fruitprices = {
+    cantaloupe: 3.99,
+    watermelon: 5.99,
+    cherry: 2.49,
+    organic: true 
+
+}
+
+console.log (Fruitprices);
+
+//Arithmetic Operator
+console.log(7 * 3); // Multiplication,
+console.log(8/ 2); // Division
+console.log(10 % 3); // Modulus (Remainder)
+
+console.log(9 + 2); // Addition
+console.log(8 - 2); // Subtraction
+
+//comparison Operators
+let x = 5; 
+let y = 10;
+console.log("10" == y); // Equality
+console.log("10" === 3); // Strict equality
+console.log(x != y); // Inequality
+console.log(x < y); // Less than
+console.log(x > y); // Greater than
+console.log(x <= y); // Less than or equal to
+console.log(x >= y); // Greater than or equal to
+
+//Logical Operators
+let a = true;
+let b = false;
+console.log(a && b); // Logical AND
+console.log(a || b); // Logical OR
+console.log(!a); // Logical NOT
